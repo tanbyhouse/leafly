96855dedde7652f0d29e7d577a535d340b8d62c2
Siti Raudatul Jannah <114587521+staudaa@users.noreply.github.com>
2025-11-21 08:47:56 +0700

    migration

---
+++ database/migrations/2025_11_21_012608_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('shipping_address_id')->constrained('shipping_addresses');
+
+            // salinan alamat saat checkout
+            $table->string('receiver_name');
+            $table->string('receiver_phone');
+            $table->text('full_address');
+
+            $table->decimal('total_price', 12, 2);
+            $table->enum('payment_method', ['cod', 'transfer']);
+            $table->enum('status', ['pending', 'paid', 'processing', 'shipped', 'completed', 'cancelled'])
+                ->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2025_11_21_012626_create_shipment_tracks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipment_tracks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shipment_id')->constrained('shipments')->onDelete('cascade');
+            $table->string('location');
+            $table->string('status_text');
+            $table->timestamp('scan_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipment_tracks');
+    }
+};
