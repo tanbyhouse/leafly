632d711ba2b4fa0c9dd32d27dc89812d3d7b1ad1
Tan <aliyaliyaa00@gmail.com>
2025-12-14 13:50:39 +0700

    delete menu chat, update links pengguna

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -57,7 +57,7 @@
 
                 <p class="px-4 text-xs font-bold text-gray-400 uppercase mt-4 mb-2">Manajemen</p>
 
-                <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-white/10 transition">
+                <a href="{{ route('admin.users.index') }}" class="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-white/10 transition">
                     <i class="fa-solid fa-users w-5 text-center"></i> Pengguna
                 </a>
 
@@ -82,9 +82,6 @@
 
                 <a href="{{ route('admin.laporan.index') }}" class="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-white/10 transition {{ request()->routeIs('admin.reports.index') ? 'bg-white/10 text-white font-bold' : '' }}">
                     <i class="fa-solid fa-chart-line w-5 text-center"></i> Laporan
-                </a>
-                <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-white/10 transition">
-                    <i class="fa-solid fa-comments w-5 text-center"></i> Chat
                 </a>
             </nav>
 
