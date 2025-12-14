c226372a5f4b0b67a2064362cb7571f339f3b433
Siti Raudatul Jannah <114587521+staudaa@users.noreply.github.com>
2025-12-14 18:17:15 +0700

    backup before migration refactor

--- routes/web.php
+++ routes/web.php
@@ -7,29 +7,20 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+Route::get('/', function () {return view('welcome');})->name('home');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
-Route::get('/katalog', [
-    ProductController::class, 'index'
-])->name('products.index');
-Route::get('/katalog/{id}', [
-    ProductController::class, 'show'
-])->name('products.show');
-Route::get('/keranjang', [
-    CartController::class, 'index'
-])->name('cart.index');
-Route::get('/checkout', [
-    CheckoutController::class, 'index'
-<<<<<<< Updated upstream
-])->name('checkout.index');
-=======
-])->name('checkout.index');
->>>>>>> Stashed changes
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/register', function () {return view('auth.register');})->name('register');
+Route::get('/katalog', [ProductController::class, 'index'])->name('products.index');
+Route::get('/katalog/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/keranjang', [CartController::class, 'index'])->name('cart.index');
+Route::get('/cart/count', [CartController::class, 'count'])->name('cart.count');
+Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
