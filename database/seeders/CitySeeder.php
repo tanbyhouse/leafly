b39e61ea44e337dd017c93ea5dec67a492cfd9f1
Siti Raudatul Jannah <114587521+staudaa@users.noreply.github.com>
2025-12-15 22:25:15 +0700

    update CitySeeder

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -5,17 +5,22 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\City;
+use App\Models\Province;
 
 class CitySeeder extends Seeder
 {
     public function run()
     {
+        $jatim = Province::where('name', 'Jawa Timur')->first();
+        $jateng = Province::where('name', 'Jawa Tengah')->first();
+        $jabar = Province::where('name', 'Jawa Barat')->first();
+
         City::insert([
-            ['province_id' => 1, 'name' => 'Jember'],
-            ['province_id' => 1, 'name' => 'Surabaya'],
-            ['province_id' => 1, 'name' => 'Malang'],
-            ['province_id' => 2, 'name' => 'Semarang'],
-            ['province_id' => 3, 'name' => 'Bandung'],
+            ['province_id' => $jatim->id, 'name' => 'Jember'],
+            ['province_id' => $jatim->id, 'name' => 'Surabaya'],
+            ['province_id' => $jatim->id, 'name' => 'Malang'],
+            ['province_id' => $jateng->id, 'name' => 'Semarang'],
+            ['province_id' => $jabar->id, 'name' => 'Bandung'],
         ]);
     }
 }
