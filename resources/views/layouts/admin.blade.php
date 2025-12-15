c3deda8219a33a951182b66a4c08d7bd4e7a9c0a
Tan <aliyaliyaa00@gmail.com>
2025-12-16 03:05:04 +0700

    fixed admin sidebar menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -75,7 +75,7 @@
                 </div>
 
                 <a href="{{ route('admin.transactions.index') }}" class="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-white/10 transition {{ request()->routeIs('admin.transactions.*') ? 'bg-white/10 text-white font-bold' : '' }}">
-                   <i class="fa-solid fa-file-invoice-dollar w-5 text-center"></i> Transaksi
+                   <i class="fa-solid fa-file-invoice-dollar w-5 text-center"></i> Pesanan
                 </a>
 
                 <p class="px-4 text-xs font-bold text-gray-400 uppercase mt-4 mb-2">Lainnya</p>
