97bdf22345e8faab024eb0601471cb28e3ac45df
Tan <aliyaliyaa00@gmail.com>
2025-12-16 01:45:19 +0700

    import RoleMiddleware from raw

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (!$user) {
+            abort(403, 'Unauthorized');
+        }
+
+        if (!$user->hasAnyRole($roles)) {
+            abort(403, 'Access denied');
+        }
+
+        return $next($request);
+    }
+}
